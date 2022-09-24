fc02a5034a0ed17a599400d7095228b357fa21f2
dipakz <deepzende876@gmail.com>
2022-09-24 10:21:20 +0530

    Initial commit

---
+++ src/employeeManagement/Conn.java
@@ -0,0 +1,18 @@
+package employeeManagement;
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagementsystem", "root", "Deepak@307");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
